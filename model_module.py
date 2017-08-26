8ac72e470e27c987fd59a25f4ce05894753e45fe
PeganovAnton <peganoff2@mail.ru>
2017-08-26 08:38:04 +0300

    fixed bug in char2id function. Now it should print unexpected characters correctly

--- model_module.py
+++ model_module.py
@@ -120,7 +120,7 @@ def char2id(char, characters_positions_in_vocabulary):
   if char in characters_positions_in_vocabulary:
     return characters_positions_in_vocabulary[char]
   else:
-    print(u'Unexpected character: %s\nUnexpected character number: %s\nUnexpected character has its place = %s\n' % (char, ord(char), present_characters_indices[i]))
+    print(u'Unexpected character: %s\nUnexpected character number: %s\n' 
     return None
 
 def char2vec(char, characters_positions_in_vocabulary):
