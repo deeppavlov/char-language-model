fix bug in loss computation. Block gradient flow in several places assigned names to tensors

chit_chat/environment.py
@@ -120,82 +120,102 @@ class Controller(object):
 
 
 def perplexity_tensor(**kwargs):
-    probabilities = kwargs['probabilities']
-    labels = kwargs['labels']
-    special_args = kwargs['special_args']
-    if 'dialog_switch' in special_args:
-        if special_args['dialog_switch']:
-            probabilities_shape = probabilities.get_shape().as_list()
-            length = probabilities_shape[1]
-            _, switch = tf.split(labels, [length, 1], axis=1)
-            switch = tf.reshape(switch, [-1])
-    ln2 = np.log(2)
-    shape = probabilities.get_shape().as_list()
-    probabilities = tf.where(probabilities > 1e-10, probabilities, np.full(tuple(shape), 1e-10))
-    log_probabilities = tf.log(probabilities) / ln2
-    entropy = tf.reduce_sum(- probabilities * log_probabilities, axis=1)
-    perplexity = tf.exp(ln2 * entropy)
-    if 'dialog_switch' in special_args:
-        if special_args['dialog_switch']:
-            perplexity = perplexity * switch
-            num_of_sensible_results = tf.reduce_sum(switch)
-            there_is_sensible = tf.not_equal(num_of_sensible_results, 0.)
-            mean_perplexity = tf.divide(tf.reduce_sum(perplexity, name='sum_perplexity'),
-                                        (num_of_sensible_results + 1e-12),
-                                        name='mean_perplexity')
-            return tf.where(there_is_sensible, mean_perplexity, -1.)
-    return tf.reduce_mean(perplexity, name="mean_perplexity")
+    with tf.name_scope('computing_perplexity'):
+        probabilities = kwargs['probabilities']
+        labels = kwargs['labels']
+        special_args = kwargs['special_args']
+        if 'dialog_switch' in special_args:
+            if special_args['dialog_switch']:
+                probabilities_shape = probabilities.get_shape().as_list()
+                length = probabilities_shape[1]
+                _, switch = tf.split(labels, [length, 1], axis=1, name='switch_vector')
+                switch = tf.reshape(switch, [-1], name='switch_reshaped')
+        ln2 = np.log(2, dtype=np.float32)
+        shape = probabilities.get_shape().as_list()
+        probabilities = tf.where(probabilities > 1e-10,
+                                 probabilities,
+                                 np.full(tuple(shape), 1e-10),
+                                 name='to_small_values_in_probs_are_filtered')
+        log_probabilities = tf.divide(tf.log(probabilities), ln2, name='log2_probs')
+        entropy = tf.reduce_sum(- probabilities * log_probabilities, axis=1, name='entropy_not_mean')
+        perplexity = tf.exp(ln2 * entropy, name='perplexity_not_aver')
+        if 'dialog_switch' in special_args:
+            if special_args['dialog_switch']:
+                perplexity = tf.multiply(perplexity, switch, name='relevant_perplexity')
+                num_of_sensible_results = tf.reduce_sum(switch, name='num_of_sensible_results')
+                there_is_sensible = tf.not_equal(num_of_sensible_results, 0., name='there_is_sensible')
+                mean_perplexity = tf.divide(tf.reduce_sum(perplexity, name='sum_perplexity'),
+                                            (num_of_sensible_results + 1e-12),
+                                            name='mean_perplexity')
+                return tf.where(there_is_sensible, mean_perplexity, -1., name='perplexity')
+        return tf.reduce_mean(perplexity, name="perplexity")
 
 
 def loss_tensor(**kwargs):
-    predictions = kwargs['predictions']
-    labels = kwargs['labels']
-    special_args = kwargs['special_args']
-    if 'dialog_switch' in special_args:
-        if special_args['dialog_switch']:
-            predictions_shape = predictions.get_shape().as_list()
-            length = predictions_shape[1]
-            labels, switch = tf.split(labels, [length, 1], axis=1)
-            switch = tf.reshape(switch, [-1])
-    shape = predictions.get_shape().as_list()
-    predictions = tf.where(predictions > 1e-10, predictions, np.full(tuple(shape), 1e-10))
-    log_predictions = tf.log(predictions)
-    loss_on_characters = tf.reduce_sum(-labels * log_predictions, axis=1)
-    if 'dialog_switch' in special_args:
-        if special_args['dialog_switch']:
-            loss_on_characters = loss_on_characters * switch
-            num_of_sensible_results = tf.reduce_sum(switch)
-            there_is_sensible = tf.not_equal(num_of_sensible_results, 0.)
-            mean_loss = tf.reduce_sum(loss_on_characters, name='mean_perplexity') / (num_of_sensible_results + 1e-12)
-            return tf.where(there_is_sensible, mean_loss, -1.)
-    return tf.reduce_mean(loss_on_characters)
+    with tf.name_scope('computing_loss'):
+        predictions = kwargs['predictions']
+        labels = kwargs['labels']
+        special_args = kwargs['special_args']
+        if 'dialog_switch' in special_args:
+            if special_args['dialog_switch']:
+                predictions_shape = predictions.get_shape().as_list()
+                length = predictions_shape[1]
+                labels, switch = tf.split(labels, [length, 1], axis=1, name='labels_and_switch')
+                switch = tf.reshape(switch, [-1], name='switch_reshaped')
+        shape = predictions.get_shape().as_list()
+        predictions = tf.where(predictions > 1e-10,
+                               predictions,
+                               np.full(tuple(shape), 1e-10),
+                               name='to_small_values_in_probs_are_filtered')
+        log_predictions = tf.log(predictions, name='log_pred')
+        loss_on_characters = tf.reduce_sum(-labels * log_predictions, axis=1, name='loss_not_mean')
+        if 'dialog_switch' in special_args:
+            if special_args['dialog_switch']:
+                loss_on_characters = tf.multiply(loss_on_characters, switch, name='relevant_loss')
+                num_of_sensible_results = tf.reduce_sum(switch, name='num_of_sensible_results')
+                there_is_sensible = tf.not_equal(num_of_sensible_results, 0., name='there_is_sensible')
+                mean_loss = tf.divide(tf.reduce_sum(loss_on_characters, name='loss_sum'),
+                                      (num_of_sensible_results + 1e-12),
+                                      name='mean_loss')
+                return tf.where(there_is_sensible, mean_loss, -1., name='loss')
+        return tf.reduce_mean(loss_on_characters, name='loss')
 
 
 def bpc_tensor(**kwargs):
-    return kwargs['loss'] / np.log(2)
+    with tf.name_scope('computing_bpc'):
+        return kwargs['loss'] / np.log(2)
 
 
 def accuracy_tensor(**kwargs):
-    predictions = kwargs['predictions']
-    labels = kwargs['labels']
-    special_args = kwargs['special_args']
-    if 'dialog_switch' in special_args:
-        if special_args['dialog_switch']:
-            predictions_shape = predictions.get_shape().as_list()
-            length = predictions_shape[1]
-            labels, switch = tf.split(labels, [length, 1], axis=1)
-            switch = tf.reshape(switch, [-1])
-    predictions = tf.argmax(predictions, axis=1)
-    labels = tf.argmax(labels, axis=1)
-    accuracy = tf.to_float(tf.equal(predictions, labels))
-    if 'dialog_switch' in special_args:
-        if special_args['dialog_switch']:
-            accuracy = accuracy * switch
-            num_of_sensible_results = tf.reduce_sum(switch)
-            there_is_sensible = tf.not_equal(num_of_sensible_results, 0.)
-            mean_accuracy = tf.reduce_sum(accuracy, name='mean_perplexity') / (num_of_sensible_results + 1e-12)
-            return tf.where(there_is_sensible, mean_accuracy, -1.)
-    return tf.reduce_mean(accuracy)
+    with tf.name_scope('computing_accuracy'):
+        predictions = kwargs['predictions']
+        labels = kwargs['labels']
+        special_args = kwargs['special_args']
+        if 'dialog_switch' in special_args:
+            if special_args['dialog_switch']:
+                predictions_shape = predictions.get_shape().as_list()
+                length = predictions_shape[1]
+                labels, switch = tf.split(labels,
+                                          [length, 1],
+                                          axis=1,
+                                          name='labels_and_switch')
+                switch = tf.reshape(switch, [-1], name='switch')
+        predictions = tf.argmax(predictions, axis=1, name='predictions')
+        labels = tf.argmax(labels, axis=1, name='labels')
+        accuracy = tf.to_float(tf.equal(predictions, labels), name='accuracy_not_aver')
+        if 'dialog_switch' in special_args:
+            if special_args['dialog_switch']:
+                accuracy = tf.multiply(accuracy, switch, name='accuracy_relevant')
+                num_of_sensible_results = tf.reduce_sum(switch, name='num_of_sensible_results')
+                # num_of_sensible_results = tf.Print(num_of_sensible_results,
+                #                                    [num_of_sensible_results],
+                #                                    message='Number of sensible results in accuracy:')
+                there_is_sensible = tf.not_equal(num_of_sensible_results, 0., name='there_is_sensible')
+                mean_accuracy = tf.divide(tf.reduce_sum(accuracy, name='accuracy_sum'),
+                                          (num_of_sensible_results + 1e-12),
+                                          name='mean_accuracy')
+                return tf.where(there_is_sensible, mean_accuracy, -1., name='accuracy')
+        return tf.reduce_mean(accuracy, name='accuracy')
 
 
 def identity_tensor(**kwargs):

chit_chat/lstm_sample_par.py
@@ -290,31 +290,45 @@ class Lstm(Model):
         return hs
 
     def _generate_random(self, shape, one_prob):
-        un = tf.random_uniform(shape, maxval=1.)
-        return tf.to_float(un < one_prob, name='random_modifier')
+        with tf.name_scope('generate_random'):
+            un = tf.random_uniform(shape, maxval=1.)
+            return tf.to_float(un < one_prob, name='random_modifier')
 
     def _decide_force_or_sample(self,
                                 inp,
                                 predictions,
                                 in_s_flags):
         with tf.name_scope('force_or_sample'):
-            answer = tf.reduce_max(predictions, axis=1, keep_dims=True)
-            answer = tf.to_float(tf.equal(predictions, answer))
+            answer = tf.reduce_max(predictions, axis=1, keep_dims=True, name='highest_scores_in_preds')
+            answer = tf.to_float(tf.equal(predictions, answer, name='one_hot_mask_in_preds'), name='labels_of_predictions')
             current_batch_size = answer.get_shape().as_list()[0]
-            speaker_flags = tf.slice(inp, [0, self._vocabulary_size], [current_batch_size, 2], name='sliced_flags')
+            speaker_flags = tf.slice(inp,
+                                     [0, self._vocabulary_size],
+                                     [current_batch_size, 2],
+                                     name='sliced_flags')
             answer = tf.concat([answer, speaker_flags], 1, name='sampled_answer')
             delimiter_vec = char2vec('\n',
                                      self._character_positions_in_vocabulary,
                                      0,
                                      2)
-            inp_to_comp = tf.slice(inp, [0, 0], [current_batch_size, self._vocabulary_size])
+            inp_to_comp = tf.slice(inp,
+                                   [0, 0],
+                                   [current_batch_size, self._vocabulary_size],
+                                   name='input_to_comp_with_delim_vec')
             [delim_to_comp, _] = np.split(delimiter_vec, [self._vocabulary_size], axis=1)
-            input_is_not_delimiter = tf.not_equal(tf.reduce_sum(inp_to_comp * delim_to_comp, axis=1, keep_dims=True), 1.)
+            input_is_not_delimiter = tf.not_equal(
+                tf.reduce_sum(inp_to_comp * delim_to_comp,
+                              axis=1,
+                              keep_dims=True),
+                1.,
+                name='input_is_not_delimiter')
             random_modifier = self._generate_random(in_s_flags.get_shape().as_list(), self.sampling_prob)
             in_s_flags = tf.multiply(in_s_flags, random_modifier, name='in_s_flags_after_modification')
-            in_s_flags = tf.cast(in_s_flags, tf.bool)
-            mask = tf.to_float(tf.logical_and(in_s_flags, input_is_not_delimiter), name='mask')
-            return tf.add(mask * answer, (1. - mask) * inp, name='inp_after_choosing')
+            in_s_flags = tf.cast(in_s_flags, tf.bool, name='in_s_flags_am_bool')
+            mask = tf.to_float(tf.logical_and(in_s_flags, input_is_not_delimiter),
+                               name='mask')
+            return tf.stop_gradient(tf.add(mask * answer, (1. - mask) * inp), name='inp_after_choosing')
+            # return tf.add((1. - mask) * answer, mask * inp, name='inp_after_choosing')
 
     def _iter(self, inp, all_states, last_predictions, in_s_flags, iter_idx):
         with tf.name_scope('iter_%s' % iter_idx):
@@ -403,12 +417,12 @@ class Lstm(Model):
         preds = list()
         losses = list()
         num_active = list()
-        for gpu_batch_size, gpu_name, device_inputs, device_labels, dev_in_s_flags, dev_out_s_flags in zip(
-                self._batch_sizes_on_gpus, self._gpu_names, self._inputs_by_device,
-                self._labels_by_device, self._in_s_flags_by_device, self._out_s_flags_by_device):
-            with tf.device(gpu_name):
-                with tf.name_scope(device_name_scope(gpu_name)):
-                    with tf.name_scope('train'):
+        with tf.name_scope('train'):
+            for gpu_batch_size, gpu_name, device_inputs, device_labels, dev_in_s_flags, dev_out_s_flags in zip(
+                    self._batch_sizes_on_gpus, self._gpu_names, self._inputs_by_device,
+                    self._labels_by_device, self._in_s_flags_by_device, self._out_s_flags_by_device):
+                with tf.device(gpu_name):
+                    with tf.name_scope(device_name_scope(gpu_name)):
                         saved_states = list()
                         for layer_idx, layer_num_nodes in enumerate(self._num_nodes):
                             saved_states.append(
@@ -443,68 +457,76 @@ class Lstm(Model):
                         logits = tf.concat(list_of_logits, 0, name='all_logits')
 
                         with tf.control_dependencies(save_ops):
-                            all_matrices = [self._embedding_matrix]
-                            all_matrices.extend(self._lstm_matrices)
-                            all_matrices.extend(self._output_matrices)
-                            l2_loss = self._l2_loss(all_matrices)
-                            random_modifier = self._generate_random(dev_out_s_flags.get_shape().as_list(),
-                                                                    (1. - self.loss_comp_prob))
-                            dev_out_s_flags = tf.subtract(1.,
-                                                          (1. - dev_out_s_flags) * random_modifier,
-                                                          name='final_dev_out_s_flags')
+                            with tf.name_scope('device_gradient_computation'):
+                                all_matrices = [self._embedding_matrix]
+                                all_matrices.extend(self._lstm_matrices)
+                                all_matrices.extend(self._output_matrices)
+                                l2_loss = self._l2_loss(all_matrices)
+                                random_modifier = self._generate_random(dev_out_s_flags.get_shape().as_list(),
+                                                                        (1. - self.loss_comp_prob))
+                                dev_out_s_flags = tf.stop_gradient(
+                                    tf.subtract(1.,
+                                                (1. - dev_out_s_flags) * random_modifier),
+                                    name='final_dev_out_s_flags')
+                                dev_out_s_flags = tf.reshape(dev_out_s_flags, [-1], name='dev_out_s_flags_reshaped')
+                                number_of_considered_losses = tf.reduce_sum(dev_out_s_flags,
+                                                                            name='number_of_considered_losses')
+                                # ce = tf.reduce_mean(
+                                #     tf.nn.softmax_cross_entropy_with_logits(labels=device_labels, logits=logits))
+                                # loss = tf.reduce_sum(ce * dev_out_s_flags) / (number_of_considered_losses + 1e-12)
+                                # losses.append(loss)
 
-                            number_of_considered_losses = tf.reduce_sum(dev_out_s_flags)
-                            # ce = tf.reduce_mean(
-                            #     tf.nn.softmax_cross_entropy_with_logits(labels=device_labels, logits=logits))
-                            # loss = tf.reduce_sum(ce * dev_out_s_flags) / (number_of_considered_losses + 1e-12)
-                            # losses.append(loss)
+                                num_active.append(number_of_considered_losses)
+                                ce = tf.nn.softmax_cross_entropy_with_logits(
+                                    labels=device_labels,
+                                    logits=logits,
+                                    name='ce_not_filtered')
+                                loss = tf.reduce_sum(ce * dev_out_s_flags, name='loss')
+                                losses.append(loss)
 
-                            num_active.append(number_of_considered_losses)
-                            ce = tf.reduce_mean(
-                                tf.nn.softmax_cross_entropy_with_logits(labels=device_labels, logits=logits))
-                            loss = tf.reduce_sum(ce * dev_out_s_flags)
-                            losses.append(loss)
+                                # optimizer = tf.train.GradientDescentOptimizer(self.learning_rate)
+                                optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
+                                grads_and_vars = optimizer.compute_gradients(loss + l2_loss)
+                                tower_grads.append(grads_and_vars)
 
-                            # optimizer = tf.train.GradientDescentOptimizer(self.learning_rate)
-                            optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
-                            grads_and_vars = optimizer.compute_gradients(loss + l2_loss)
-                            tower_grads.append(grads_and_vars)
+                                # splitting concatenated results for different characters
+                                concat_pred = tf.nn.softmax(logits)
+                                preds.append(tf.split(concat_pred, self._num_unrollings))
 
-                            # splitting concatenated results for different characters
-                            concat_pred = tf.nn.softmax(logits)
-                            preds.append(tf.split(concat_pred, self._num_unrollings))
+            with tf.device('/cpu:0'):
+                with tf.name_scope(device_name_scope('/cpu:0') + '_gradients'):
+                    # grads_and_vars = average_gradients(tower_grads)
+                    grads_and_vars = average_gradients_not_balanced(tower_grads, num_active)
 
-        with tf.device('/cpu:0'):
-            with tf.name_scope(device_name_scope('/cpu:0') + '_gradients'):
-                # grads_and_vars = average_gradients(tower_grads)
-                grads_and_vars = average_gradients_not_balanced(tower_grads, num_active)
+                    grads, v = zip(*grads_and_vars)
+                    grads, _ = tf.clip_by_global_norm(grads, 1.)
+                    optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
+                    self.train_op = optimizer.apply_gradients(zip(grads, v))
+                    self._hooks['train_op'] = self.train_op
 
-                grads, v = zip(*grads_and_vars)
-                grads, _ = tf.clip_by_global_norm(grads, 1.)
-                optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
-                self.train_op = optimizer.apply_gradients(zip(grads, v))
-                self._hooks['train_op'] = self.train_op
+                    # composing predictions
+                    preds_by_char = list()
+                    # print('preds:', preds)
+                    for char_idx, one_char_preds in enumerate(zip(*preds)):
+                        # print('one_char_preds:', one_char_preds)
+                        preds_by_char.append(
+                            tf.concat(
+                                one_char_preds, 0,
+                                name='one_char_preds_%s' % char_idx))
+                    # print('len(preds_by_char):', len(preds_by_char))
+                    self.predictions = tf.concat(preds_by_char, 0, name='predictions')
+                    self._hooks['predictions'] = self.predictions
 
-                # composing predictions
-                preds_by_char = list()
-                # print('preds:', preds)
-                for one_char_preds in zip(*preds):
-                    # print('one_char_preds:', one_char_preds)
-                    preds_by_char.append(tf.concat(one_char_preds, 0))
-                # print('len(preds_by_char):', len(preds_by_char))
-                self.predictions = tf.concat(preds_by_char, 0)
-                self._hooks['predictions'] = self.predictions
+                    # print('self.predictions.get_shape().as_list():', self.predictions.get_shape().as_list())
+                    # l = 0
+                    # for loss, gpu_batch_size in zip(losses, self._batch_sizes_on_gpus):
+                    #     l += float(gpu_batch_size) / float(self._batch_size) * loss
 
-                # print('self.predictions.get_shape().as_list():', self.predictions.get_shape().as_list())
-                # l = 0
-                # for loss, gpu_batch_size in zip(losses, self._batch_sizes_on_gpus):
-                #     l += float(gpu_batch_size) / float(self._batch_size) * loss
-
-                l = tf.identity(sum(losses), name='sum_of_all_losses')
-                num_active = tf.identity(sum(num_active), name='number_of_computed_losses')
-                l /= (num_active + 1e-12)
-                self.loss = l
-                self._hooks['loss'] = self.loss
+                    l = tf.identity(sum(losses), name='sum_of_all_losses')
+                    num_active = tf.identity(sum(num_active), name='number_of_computed_losses')
+                    # num_active = tf.Print(num_active, [l, num_active], message='loss_sum, num_active:')
+                    self.loss = tf.divide(l, (num_active + 1e-12), name='average_loss')
+                    self._hooks['loss'] = self.loss
 
     def _validation_graph(self):
         with tf.device(self._gpu_names[0]):

chit_chat/some_useful_functions.py
@@ -465,29 +465,30 @@ def average_gradients(tower_grads):
        List of pairs of (gradient, variable) where the gradient has been averaged
        across all towers.
     """
-    average_grads = []
-    for grad_and_vars in zip(*tower_grads):
-        # Note that each grad_and_vars looks like the following:
-        #   ((grad0_gpu0, var0_gpu0), ... , (grad0_gpuN, var0_gpuN))
-        grads = []
-        for g, _ in grad_and_vars:
-            # Add 0 dimension to the gradients to represent the tower.
-            expanded_g = tf.expand_dims(g, 0)
+    with tf.name_scope('average_gradients'):
+        average_grads = []
+        for grad_and_vars in zip(*tower_grads):
+            # Note that each grad_and_vars looks like the following:
+            #   ((grad0_gpu0, var0_gpu0), ... , (grad0_gpuN, var0_gpuN))
+            grads = []
+            for g, _ in grad_and_vars:
+                # Add 0 dimension to the gradients to represent the tower.
+                expanded_g = tf.expand_dims(g, 0)
 
-            # Append on a 'tower' dimension which we will average over below.
-            grads.append(expanded_g)
+                # Append on a 'tower' dimension which we will average over below.
+                grads.append(expanded_g)
 
-        # Average over the 'tower' dimension.
-        grad = tf.concat(axis=0, values=grads)
-        grad = tf.reduce_mean(grad, 0)
+            # Average over the 'tower' dimension.
+            grad = tf.concat(axis=0, values=grads)
+            grad = tf.reduce_mean(grad, 0)
 
-        # Keep in mind that the Variables are redundant because they are shared
-        # across towers. So .. we will just return the first tower's pointer to
-        # the Variable.
-        v = grad_and_vars[0][1]
-        grad_and_var = (grad, v)
-        average_grads.append(grad_and_var)
-    return average_grads
+            # Keep in mind that the Variables are redundant because they are shared
+            # across towers. So .. we will just return the first tower's pointer to
+            # the Variable.
+            v = grad_and_vars[0][1]
+            grad_and_var = (grad, v)
+            average_grads.append(grad_and_var)
+        return average_grads
 
 
 def average_gradients_not_balanced(tower_grads, num_active):
@@ -501,29 +502,30 @@ def average_gradients_not_balanced(tower_grads, num_active):
        List of pairs of (gradient, variable) where the gradient has been averaged
        across all towers.
     """
-    average_grads = []
-    for grad_and_vars in zip(*tower_grads):
-        # Note that each grad_and_vars looks like the following:
-        #   ((grad0_gpu0, var0_gpu0), ... , (grad0_gpuN, var0_gpuN))
-        grads = []
-        for g, _ in grad_and_vars:
-            # Add 0 dimension to the gradients to represent the tower.
-            expanded_g = tf.expand_dims(g, 0)
+    with tf.name_scope('not_balanced_average'):
+        average_grads = []
+        for grad_and_vars in zip(*tower_grads):
+            # Note that each grad_and_vars looks like the following:
+            #   ((grad0_gpu0, var0_gpu0), ... , (grad0_gpuN, var0_gpuN))
+            grads = []
+            for g, _ in grad_and_vars:
+                # Add 0 dimension to the gradients to represent the tower.
+                expanded_g = tf.expand_dims(g, 0)
 
-            # Append on a 'tower' dimension which we will average over below.
-            grads.append(expanded_g)
+                # Append on a 'tower' dimension which we will average over below.
+                grads.append(expanded_g)
 
-        # Average over the 'tower' dimension.
-        grad = tf.concat(axis=0, values=grads)
-        grad = tf.reduce_sum(grad, 0) / sum(num_active)
+            # Average over the 'tower' dimension.
+            grad = tf.concat(axis=0, values=grads)
+            grad = tf.reduce_sum(grad, 0) / sum(num_active)
 
-        # Keep in mind that the Variables are redundant because they are shared
-        # across towers. So .. we will just return the first tower's pointer to
-        # the Variable.
-        v = grad_and_vars[0][1]
-        grad_and_var = (grad, v)
-        average_grads.append(grad_and_var)
-    return average_grads
+            # Keep in mind that the Variables are redundant because they are shared
+            # across towers. So .. we will just return the first tower's pointer to
+            # the Variable.
+            v = grad_and_vars[0][1]
+            grad_and_var = (grad, v)
+            average_grads.append(grad_and_var)
+        return average_grads
 
 
 def get_num_gpus_and_bs_on_gpus(batch_size, num_gpus, num_available_gpus):